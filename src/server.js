09a5a6230d16bd00ae850a3ada15763c55db3d25
EHAB1990 <ehabb1990101@gmail.com>
2026-06-27 23:31:21 +0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
-const app = require('./app');
-const { connectDB } = require('./db');
+const express = require('express');
+const cors = require('cors');
 
+const app = express();
 const PORT = process.env.PORT || 10000;
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}).catch(err => {
-  console.error('Database connection failed', err);
-  process.exit(1);
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'AseeL Store API is running!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
