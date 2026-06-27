b4dab8fa52a41a182d2560b63251f628674a2811
EHAB1990 <ehabb1990101@gmail.com>
2026-06-27 23:25:50 +0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
+const app = require('./app');
+const { connectDB } = require('./db');
 
+const PORT = process.env.PORT || 10000;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}).catch(err => {
+  console.error('Database connection failed', err);
+  process.exit(1);
+});
